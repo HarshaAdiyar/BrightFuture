0a922a932e727b7aab5e14aeebd962d23a8eb3b8
Tanushreems <tanushreems62@gmail.com>
2025-08-08 22:45:40 +0530

    modified

---
+++ src/main/java/com/protsaha/brightfuture/model/Aadhar_Info.java
@@ -0,0 +1,12 @@
+package com.protsaha.brightfuture.model;
+
+import lombok.Data;
+
+
+
+@Data
+public class Aadhar_Info {
+    private String studentName;
+    private String studentAadharNo;
+    private String studentRdNo;
+}
