9a9439853d68dc64729ad3b1e185c913998b76a9
Mahalakshmi R K <your-rkmahalakshmi8@example.com>
2025-08-08 20:17:13 +0530

    modified

--- src/main/java/com/protsaha/brightfuture/controller/StudentController.java
+++ src/main/java/com/protsaha/brightfuture/controller/StudentController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,8 +35,6 @@ public class StudentController {
     @PostMapping
     @Operation(summary = "Add a New Student", description = "Add a new student to the database.")
     public Student addStudent(@RequestBody Student student) {
-        System.out.println("addStudent");
-        System.out.println("print Student  "+student.toString());
         return studentService.addStudent(student);
     }
 
--- src/main/java/com/protsaha/brightfuture/model/Student.java
+++ src/main/java/com/protsaha/brightfuture/model/Student.java
@@ -1,36 +1,28 @@
 package com.protsaha.brightfuture.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Document(collection = "Student")
 @Data
 public class Student {
     @Id
-    private String studentId;
+    @JsonProperty("studentId")
+    private String id;
     private String name;
     private int age;
+    private String email;
+    private String course;
     private String dob;
-    private String aadhar;
-    private ContactDetails contactDetails;
+    private String contactNumber1;
+    private String contactNumber2;
+    private AadharInfo aadharInfo;
+    private Admission admission;
+    private PersonalDetails personalDetails;
     private Address address;
-    private Grade grade;
     private Parents parents;
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id='" + studentId + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", dob='" + dob + '\'' +
-                ", aadhar='" + aadhar + '\'' +
-                ", contactDetails=" + contactDetails +
-                ", address=" + address +
-                ", grade=" + grade +
-                ", parents=" + parents +
-                '}';
-    }
-    // Getters and Setters (Omitted for brevity)
+    private PreviousSchool previousSchool;
 }
