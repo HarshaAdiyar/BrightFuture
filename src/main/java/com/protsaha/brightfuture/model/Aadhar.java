779b104d73d0fc7b26dfad2be6a146b77b943bc8
Vinayaka K C <vinayakakc28@gmail.com>
2025-08-08 18:43:16 +0530

    Updated models and configuration in Vinay branch

---
+++ src/main/java/com/protsaha/brightfuture/model/Aadhar.java
@@ -0,0 +1,12 @@
+package com.protsaha.brightfuture.model;
+
+import lombok.Data;
+
+
+
+@Data
+public class Aadhar {
+    private String studentName;
+    private String studentAadharNo;
+    private String studentRdNo;
+}
