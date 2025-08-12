64ec331516b9a2a6b26bacfc7b7fd4eaf7bf6314
Anni <142305502+Annishivakumara@users.noreply.github.com>
2025-08-12 21:09:37 +0530

    Update BrightFutureApplication.java

--- src/main/java/com/protsaha/brightfuture/BrightFutureApplication.java
+++ src/main/java/com/protsaha/brightfuture/BrightFutureApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrightFutureApplication {
+
 	public static void main(String[] args) {
-        SpringApplication.run(BrightFutureApplication.class, args);
+		SpringApplication.run(BrightFutureApplication.class, args);
 	}
+
 }
