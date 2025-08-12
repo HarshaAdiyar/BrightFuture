dd55fad2885f67b5d1a73ac5457f54810f0845b9
Anni Shivakumara <ashivu656@gmail.com>
2025-08-12 22:05:10 +0530

    Updated PreviousSchool model

--- src/main/java/com/protsaha/brightfuture/model/PreviousSchool.java
+++ src/main/java/com/protsaha/brightfuture/model/PreviousSchool.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class PreviousSchool {
     private String schoolName;
-    private Address.AddressDetail address; // Reusing AddressDetail from Address class
+    private Address.AddressDetail address;
 }
