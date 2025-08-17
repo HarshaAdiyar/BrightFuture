e4a0a0bc4f201f2899784fe0f967b74e8109fe23
adarshsr2005 <adarshsr2005@gmail.com>
2025-08-17 19:28:57 +0530

    worked on UI

--- src/main/resources/static/page1.js
+++ src/main/resources/static/page1.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("studentform");
+  const form = document.getElementById("studentForm");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault(); // stop page refresh
