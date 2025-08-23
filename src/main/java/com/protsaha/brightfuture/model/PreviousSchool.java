bd5aea9cd5c43ef05625522d90d0c4eee04c26ac
adarshsr2005 <adarshsr2005@gmail.com>
2025-08-23 18:00:13 +0530

    set validations for contact no and pin etc

--- src/main/java/com/protsaha/brightfuture/model/PreviousSchool.java
+++ src/main/java/com/protsaha/brightfuture/model/PreviousSchool.java
@@ -5,9 +5,5 @@ import lombok.Data;
 @Data
 public class PreviousSchool {
     private String schoolName;
-    private String schoolContact;
-    private String schoolVillage;
-    private String schoolTaluk;
-    private String schoolDistrict;
-    private String schoolPin;
+    private Address.AddressDetail address;
 }
