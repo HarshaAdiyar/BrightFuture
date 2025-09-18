6371a7f357d2f0442ec186c8715d07ce5d5465a4
Vinayaka K C <vinayakakc28@gmail.com>
2025-09-18 14:11:27 +0530

    Teacher qualification is updated

--- src/main/java/com/protsaha/brightfuture/model/Teacher.java
+++ src/main/java/com/protsaha/brightfuture/model/Teacher.java
@@ -1,63 +1,35 @@
 package com.protsaha.brightfuture.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.List;
 
+@Document(collection = "Teacher")
+@Data
 public class Teacher {
+    @Id
     private String teacherId;
     private Employment employment;
     private TeacherAadharInfo aadharInfo;
     private PersonalDetails personalDetails;
-    private Address address;
-    private List<Qualification> qualifications;
+    private Teacher_Address address;
+    private List<Teacher_Qualification> qualifications;
     private List<Experience> experience;
 
-
-    public String getTeacherId() {
-        return teacherId;
-    }
-    public void setTeacherId(String teacherId) {
-        this.teacherId = teacherId;
-    }
-
-    public Employment getEmployment() {
-        return employment;
-    }
-    public void setEmployment(Employment employment) {
-        this.employment = employment;
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "teacherId='" + teacherId + '\'' +
+                ", employment=" + employment +
+                ", aadharInfo=" + aadharInfo +
+                ", personalDetails=" + personalDetails +
+                ", address=" + address +
+                ", qualifications=" + qualifications +
+                ", experience=" + experience +
+                '}';
     }
 
-    public TeacherAadharInfo getAadharInfo() {
-        return aadharInfo;
-    }
-    public void setAadharInfo(TeacherAadharInfo aadharInfo) {
-        this.aadharInfo = aadharInfo;
-    }
 
-    public PersonalDetails getPersonalDetails() {
-        return personalDetails;
-    }
-    public void setPersonalDetails(PersonalDetails personalDetails) {
-        this.personalDetails = personalDetails;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public List<Qualification> getQualifications() {
-        return qualifications;
-    }
-    public void setQualifications(List<Qualification> qualifications) {
-        this.qualifications = qualifications;
-    }
-
-    public List<Experience> getExperience() {
-        return experience;
-    }
-    public void setExperience(List<Experience> experience) {
-        this.experience = experience;
-    }
 }
