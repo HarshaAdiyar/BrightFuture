84947d7354c88c44a002ff534d656d22aacd7f57
Anjaneya <anjaneya0003@gmail.com>
2025-09-19 20:24:28 +0530

    Address is updated

--- src/main/java/com/protsaha/brightfuture/model/Address.java
+++ src/main/java/com/protsaha/brightfuture/model/Address.java
@@ -1,40 +1,19 @@
-// File: src/main/java/com/protsaha/brightfuture/model/Address.java
-
 package com.protsaha.brightfuture.model;
 
-// Use this if you are using Lombok
-// import lombok.Data;
+import lombok.Data;
 
-// @Data // Place the @Data annotation at the class level
+@Data
 public class Address {
-    private String street;
-    private String city;
-    private String state;
-    private String zipCode;
+    private AddressDetail parentsAddress;
+    private AddressDetail guardianAddress;
 
-    // Getters and Setters for the fields are also required if not using Lombok
-    public String getStreet() {
-        return street;
-    }
-    public void setStreet(String street) {
-        this.street = street;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getState() {
-        return state;
-    }
-    public void setState(String state) {
-        this.state = state;
-    }
-    public String getZipCode() {
-        return zipCode;
-    }
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
+    @Data
+    public static class AddressDetail {
+        private String village;
+        private String taluk;
+        private String district;
+        private String pin;
+        private String contactNo;
+
     }
 }
